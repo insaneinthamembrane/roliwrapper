cd918f5ede47e9096d510c22b0d73f6f704bf474
htach <72769068+htach@users.noreply.github.com>
2022-07-17 19:29:07 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', encoding='utf-8') as f:
 
 setup(
     name='roliwrapper',
-    version='0.0.1',
+    version='0.2.0',
     author='htach',
     description='Rolimons API Wrapper',
     long_description=readme,
