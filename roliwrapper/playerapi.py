441ea355fbe28edc4cb1530e8dcd0901f4e4a628
hatch <72769068+htach@users.noreply.github.com>
2022-09-13 20:15:06 -0500

    Update playerapi.py

--- roliwrapper/playerapi.py
+++ roliwrapper/playerapi.py
@@ -7,7 +7,6 @@ def fetch_player(player_id: int) -> Player:
     req = requests.get(f"https://www.rolimons.com/playerapi/player/{player_id}")
     res = req.json()
 
-
     if not res['success']:
         return print(f'Unsuceessful request (Reason: {res["message"]})')
 
